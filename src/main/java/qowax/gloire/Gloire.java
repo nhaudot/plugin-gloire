5343140cb45e0df15a396c5097263367ff1f99f0
Nathan Haudot <nathan.haudot@outlook.fr>
2021-07-15 20:56:42 +0200

    - [OK] Création BDD SQL automatique (database.sql) - [BUG: OK] Ajout joueur BDD

--- src/main/java/qowax/gloire/Gloire.java
+++ src/main/java/qowax/gloire/Gloire.java
@@ -3,16 +3,19 @@ package qowax.gloire;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
+
 import qowax.gloire.Listeners.EventListener;
 import qowax.gloire.Listeners.MobsListener;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public final class Gloire extends JavaPlugin {
 
     public static final String BUILD_MAJ = "1";
     public static final String BUILD_MIN = "0";
-    public static final String BUILD_NUMBER = "41";
+    public static final String BUILD_NUMBER = "63";
 
     public static Database bdd;
     public static Plugin plugin;
@@ -40,6 +43,16 @@ public final class Gloire extends JavaPlugin {
             public void run() {
                 try {
                     bdd.connect();
+
+                    // Vérifie que la BDD est initialisée
+                    ArrayList<String> result = bdd.sendRequest("SHOW TABLES LIKE 'statistiques'");
+                    if (result.size() == 0) {
+                        // On doit initialiser la base de données
+                        String sqldump = new Scanner(Gloire.class.getResourceAsStream("/database.sql"), "UTF-8").useDelimiter("\\A").next();
+                        getLogger().info("Création de la base de données...");
+                        bdd.executeQuery(sqldump);
+                        getLogger().info("Base de données ok!");
+                    }
                 } catch (SQLException throwables) {
                     getLogger().warning("Erreur MySQL : " + throwables.getMessage());
                     getLogger().warning ("Impossible de se connecter à la base de données, veuillez vérifier les informations dans le fichier config.yml");
--- src/main/java/qowax/gloire/Listeners/EventListener.java
+++ src/main/java/qowax/gloire/Listeners/EventListener.java
@@ -41,35 +41,33 @@ public class EventListener implements Listener {
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
         Player player = event.getPlayer();
-        if(!player.hasPlayedBefore()) {
-            // Fetch DDB pour retrouver si le joueur existe
-            if (bdd == null) {
-                bdd = new Database(
-                        Gloire.plugin.getConfig().getString("database.host"),
-                        Integer.parseInt(Gloire.plugin.getConfig().getString("database.port")),
-                        Gloire.plugin.getConfig().getString("database.database"),
-                        Gloire.plugin.getConfig().getString("database.username"),
-                        Gloire.plugin.getConfig().getString("database.password")
-                );
-            }
-
-            Bukkit.getScheduler().runTaskAsynchronously(Gloire.plugin, new Runnable() {
-                @Override
-                public void run() {
-                    try {
-                        bdd.connect();
-                        ArrayList<String> result = bdd.sendRequest("SELECT `joueur` FROM `statistiques` WHERE `uuid` = '" + event.getPlayer().getUniqueId() + "'");
-
-                        if (result.size() == 0) {
-                            bdd.executeQuery("INSERT INTO `statistiques`(`uuid`, `joueur`, `gloire`) VALUES ('" + event.getPlayer().getUniqueId().toString() + "','" + event.getPlayer().getName() + "','" + Gloire.plugin.getConfig().getString("config.gloire_base") + "')");
-                        }
-                    } catch (SQLException throwables) {
-                        Bukkit.getServer().getLogger().warning("Erreur MySQL : " + throwables.getMessage());
-                        Bukkit.getServer().getLogger().warning ("Impossible de se connecter à la base de données, veuillez vérifier les informations dans le fichier config.yml");
-                        Bukkit.getPluginManager().disablePlugin(Gloire.plugin);
-                    }
-                }
-            });
+        // Fetch DDB pour retrouver si le joueur existe
+        if (bdd == null) {
+            bdd = new Database(
+                    Gloire.plugin.getConfig().getString("database.host"),
+                    Integer.parseInt(Gloire.plugin.getConfig().getString("database.port")),
+                    Gloire.plugin.getConfig().getString("database.database"),
+                    Gloire.plugin.getConfig().getString("database.username"),
+                    Gloire.plugin.getConfig().getString("database.password")
+            );
         }
+
+        Bukkit.getScheduler().runTaskAsynchronously(Gloire.plugin, new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    bdd.connect();
+                    ArrayList<String> result = bdd.sendRequest("SELECT `joueur` FROM `statistiques` WHERE `uuid` = '" + event.getPlayer().getUniqueId() + "'");
+
+                    if (result.size() == 0) {
+                        bdd.executeQuery("INSERT INTO `statistiques`(`uuid`, `joueur`, `gloire`) VALUES ('" + event.getPlayer().getUniqueId().toString() + "','" + event.getPlayer().getName() + "','" + Gloire.plugin.getConfig().getString("config.gloire_base") + "')");
+                    }
+                } catch (SQLException throwables) {
+                    Bukkit.getServer().getLogger().warning("Erreur MySQL : " + throwables.getMessage());
+                    Bukkit.getServer().getLogger().warning ("Impossible de se connecter à la base de données, veuillez vérifier les informations dans le fichier config.yml");
+                    Bukkit.getPluginManager().disablePlugin(Gloire.plugin);
+                }
+            }
+        });
     }
 }
